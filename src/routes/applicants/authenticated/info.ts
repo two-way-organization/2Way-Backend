64dd1cc6c4403a993e1c9e356e0c09c607939737
JellyBrick <shlee1503@naver.com>
2023-11-14 12:09:34 +0900

    feat: add info

---
+++ src/routes/applicants/authenticated/info.ts
@@ -0,0 +1,50 @@
+import { prismaClient } from '../../../utils/prisma-client';
+
+import type { Next, ParameterizedContext } from 'koa';
+import type { ZodContext } from 'koa-zod-router';
+
+import type { JwtPayloadState } from '../../@types/jwt-payload-state';
+
+export interface InfoResponseBody {
+  id: number;
+  fullName: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface ErrorResponse {
+  message: string;
+}
+
+export const info = async (
+  ctx: ParameterizedContext<JwtPayloadState, ZodContext<unknown, unknown, unknown, unknown, unknown>, InfoResponseBody | ErrorResponse>,
+  next: Next
+) => {
+  const { id, email } = ctx.state;
+
+  const account = await prismaClient.applicants.findUnique({
+    where: {
+      id,
+      email,
+    },
+  });
+
+  if (!account) {
+    ctx.status = 404;
+    ctx.body = {
+      message: 'Account not found.',
+    };
+  } else {
+    ctx.status = 200;
+    ctx.body = {
+      id: account.id,
+      fullName: account.fullName,
+      email: account.email,
+      createdAt: account.createdAt,
+      updatedAt: account.updatedAt,
+    };
+  }
+
+  await next();
+};

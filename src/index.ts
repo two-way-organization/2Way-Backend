4815987091714821287a93f142ef7a4470ac38a2
JellyBrick <shlee1503@naver.com>
2023-11-15 17:11:12 +0900

    feat: register routes

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,37 @@ import camelCase from 'koa-camelcase-keys';
 
 import jwt from 'koa-jwt';
 
-import { unauthenticatedApplicantRoutes } from './routes/applicants';
+import cors from '@koa/cors';
+
+import { authenticatedApplicantRoutes, unauthenticatedApplicantRoutes } from './routes/applicants';
+import { authenticatedCompanyRoutes, unauthenticatedCompanyRoutes } from './routes/companies';
 
 function main() {
   const app = new koa();
 
   app.use(bodyParser());
   app.use(camelCase());
+  app.use(cors());
 
-  app.use(unauthenticatedApplicantRoutes().routes());
-  app.use(unauthenticatedApplicantRoutes().allowedMethods());
+  const unauthenticatedApplicant = unauthenticatedApplicantRoutes();
+  const unauthenticatedCompany = unauthenticatedCompanyRoutes();
+  app.use(unauthenticatedApplicant.routes());
+  app.use(unauthenticatedApplicant.allowedMethods());
+  app.use(unauthenticatedCompany.routes());
+  app.use(unauthenticatedCompany.allowedMethods());
 
   app.use(jwt({
     secret: process.env.JWT_SECRET!,
     algorithms: [process.env.JWT_ALGORITHM!],
     issuer: process.env.JWT_ISSUER!,
   }));
+  
+  const authenticatedApplicant = authenticatedApplicantRoutes();
+  const authenticatedCompany = authenticatedCompanyRoutes();
+  app.use(authenticatedApplicant.routes());
+  app.use(authenticatedApplicant.allowedMethods());
+  app.use(authenticatedCompany.routes());
+  app.use(authenticatedCompany.allowedMethods());
 
   const port = process.env.PORT!;
   app.listen(port, () => {
